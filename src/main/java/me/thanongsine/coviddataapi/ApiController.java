ab9629c3809797e5e08224fc4d30c24620f23f6c
Ted555 <thanong984@gmail.com>
2021-05-25 16:04:17 +0700

    Update information

--- src/main/java/me/thanongsine/coviddataapi/ApiController.java
+++ src/main/java/me/thanongsine/coviddataapi/ApiController.java
@@ -21,23 +21,23 @@ public class ApiController {
     public ResponseEntity<?> getCovidCases() {
         Map<String, Object> data = new HashMap<>();
         List<Province> provinceList = new ArrayList<>();
-        provinceList.add(new Province("ຜົ້ງສາລີ", 5, 0));
+        provinceList.add(new Province("ຜົ້ງສາລີ", 8, 3));
         provinceList.add(new Province("ຫຼວງນໍ້າທາ", 8, 0));
         provinceList.add(new Province("ຫົວພັນ", 0, 0));
-        provinceList.add(new Province("ບໍ່ແກ້ວ", 475, 0));
-        provinceList.add(new Province("ອຸດົມໄຊ", 14, 0));
+        provinceList.add(new Province("ບໍ່ແກ້ວ", 492, 17));
+        provinceList.add(new Province("ອຸດົມໄຊ", 22, 7));
         provinceList.add(new Province("ໄຊຍະບູລີ", 3, 0));
         provinceList.add(new Province("ຊຽງຂວາງ", 1, 0));
         provinceList.add(new Province("ຫຼວງພະບາງ", 16, 0));
         provinceList.add(new Province("ວຽງຈັນ", 18, 0));
-        provinceList.add(new Province("ນຫລ ວຽງຈັນ", 850, 4));
+        provinceList.add(new Province("ນຫລ ວຽງຈັນ", 875, 16));
         provinceList.add(new Province("ບໍລິຄຳໄຊ", 3, 0));
         provinceList.add(new Province("ຄຳມ່ວນ", 6, 5));
-        provinceList.add(new Province("ສະຫວັນນະເຂດ", 86, 5));
+        provinceList.add(new Province("ສະຫວັນນະເຂດ", 97, 10));
         provinceList.add(new Province("ສາລະວັນ", 13, 0));
         provinceList.add(new Province("ເຊກອງ", 1, 0));
         provinceList.add(new Province("ອັດຕະປື", 0, 0));
-        provinceList.add(new Province("ຈຳປາສັກ", 301, 5));
+        provinceList.add(new Province("ຈຳປາສັກ", 314, 3));
         provinceList.add(new Province("ໄຊສົມບູນ", 1, 0));
 
 
@@ -47,13 +47,13 @@ public class ApiController {
         newsList.add(new News("ຈຸດສັກວັກຊີນເຂັມ2ຢູ່ນະຄອນຫຼວງວຽງຈັນ", "https://www.facebook.com/CIEH.MoH.Lao/photos/a.1580136645344357/4275377012486960/"));
 
         data.put("newsList", newsList);
-        data.put("totalInfected", 1822);
-        data.put("newCases", 21);
+        data.put("totalInfected", 1878);
+        data.put("newCases", 56);
         data.put("provinces", provinceList);
-        data.put("active", 687);
+        data.put("active", 696);
         data.put("death", 2);
-        data.put("recovered", 1133);
-        data.put("test", 233618);
+        data.put("recovered", 1180);
+        data.put("test", 236228);
         data.put("appVersion", 0.1);
         data.put("updateURL", "https://play.google.com/store/apps/details?id=info.laoscovid19.laocovid19&hl=en&gl=US");
         data.put("infoDate", "25/05/2021");
